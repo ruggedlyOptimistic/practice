247cd858fc39a92787c774e52097d5f72461f54a
= <=>
2024-11-21 16:31:16 -0800

    updates

--- cpp_practice/stringMethods.cpp
+++ cpp_practice/stringMethods.cpp
@@ -1,6 +1,9 @@
 #include <string>
 #include <iostream>
+#include <set>
 
+
+// write a function that takes a string and reverses the order of the characters in the string
 void reverseString(std::string &myString)
 {
     char a, b;
@@ -24,6 +27,38 @@ void reverseString(std::string &myString)
     
 }
 
+// write a function to return the length of the longest substring without repeating characters
+int lengthOfLongestSubstring(std::string &myString)
+{
+    // make a counter
+    int counter, max = 1;
+    char previous;
+
+    // create a substring variable
+    for (int index = 1; index < myString.length(); index++) // we normally start the index at zero, but we can reduce the program size by starting at 1
+    {   
+        previous = myString[index-1];
+        if (myString[index] != previous)
+        {
+            // increment the counter if the current character is not equal to the previous one
+            counter++;
+        }
+
+        else
+        {
+             counter = 0;    // if it is, reset the counter to zero
+        }  
+
+        // compare the current value stored in max to the counter, update max as needed
+        if (max < counter)
+        {
+            max = counter;
+        }
+    }
+
+    return max;
+}
+
 int main()
 {
     /*
@@ -31,8 +66,12 @@ int main()
                 -> Most importantly, it does not allow printing of objects, which is probably the biggest reason for C++ (OOP support), so
                 -> modern C++ uses 'cout' as the defacto print method
     */
-    std::string myString;
     
+    std::string myString;
+    u_int8_t len;
+
+    for (;;)
+    {  
     std::cout << "Enter a word: ";
     std::cin >> myString;
     std::cout << "Original word: " << myString << "\n";
@@ -40,6 +79,9 @@ int main()
     // Reverse the string
     reverseString(myString);
     std::cout << "Reversed: " << myString << "\n";
+
+    std::cout << "Longest substring of non-repeating characters: " << lengthOfLongestSubstring(myString) << "\n";
+    }
 
     return 0;
 }
